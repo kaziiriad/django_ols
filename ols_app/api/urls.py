2c039843cc1a8cb4b01bf0243af5f943219e38de
Sultan Mahmud. <kaziiriad@gmail.com>
2024-04-02 16:57:08 +0600

    Added EnrollmentViewSet. Works perfectly.

--- ols_app/api/urls.py
+++ ols_app/api/urls.py
@@ -2,10 +2,11 @@ from django.urls import path, include
 
 from rest_framework import routers
 
-from .views import CourseViewSet
+from .views import CourseViewSet, EnrollmentViewSet
 
 router = routers.DefaultRouter()
 router.register(r'courses', CourseViewSet)
+router.register(r'enrollments', EnrollmentViewSet)
 
 
 urlpatterns = [
--- ols_app/api/views.py
+++ ols_app/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404
 
 from rest_framework.views import APIView
-from rest_framework.viewsets import ViewSet, ModelViewSet
+from rest_framework.viewsets import ViewSet, ModelViewSet, GenericViewSet
 from rest_framework.response import Response
 from rest_framework import status, filters
 from rest_framework.generics import ListAPIView
@@ -65,10 +65,24 @@ class CourseViewSet(ModelViewSet):
     serializer_class = CourseSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['instructor', 'duration_minutes', 'price_bdt']
+
+
+
+class EnrollmentViewSet(GenericViewSet):
+
+    queryset = Enrollment.objects.all()
+    serializer_class =  EnrollmentSerializer
+
+    def create(self, request, *args, **kwargs):
+        
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
     
 
 
-class EnrollmentAPIView(APIView):
 
-    pass
 
