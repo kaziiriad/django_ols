89803b32c351506066d861449f7462d0e667296f
Sultan Mahmud. <kaziiriad@gmail.com>
2024-04-03 17:09:26 +0600

    Implemented test. Works perfectly.

--- ols_app/api/urls.py
+++ ols_app/api/urls.py
@@ -8,7 +8,22 @@ router = routers.DefaultRouter()
 router.register(r'courses', CourseViewSet)
 router.register(r'enrollments', EnrollmentViewSet)
 
+# for urlpattern in router.urls:
+#     print(urlpattern)
+
 
 urlpatterns = [
     path('', include(router.urls))    
 ]
+
+"""
+course-list
+course-list
+course-detail
+course-detail
+enrollment-list
+enrollment-list
+api-root
+api-root
+
+"""
